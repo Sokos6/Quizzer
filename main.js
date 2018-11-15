f357ad48077434432fef7c24cb92ef4b1b79743c
Will Sokolowski <will.sokolowski@gmail.com>
2018-11-14 21:05:17 -0700

    game object and refactor

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-const quiz = new Map([
-  ["What is Superman's real name?", "Clark Kent"],
-  ["What is Wonderwoman's real name?", "Dianna Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"]
-]);
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonderwoman's real name?","Dianna Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+  ];
 
-let score = 0;
+function start(quiz){
+   let score = 0;
 
-for (const [question, answer] of quiz.entries()) {
-  const response = prompt(question);
-  if (response === answer) {
-    alert("Correct!");
-    score++;
-  } else {
-    alert(`Wrong! The correct answer was ${answer}`);
+  for(const [question,answer] of quiz){
+    const response = ask(question);
+    check(response,answer);
+  }
+
+  gameOver();
+
+  function ask(question){
+    return prompt(question);
+  }
+
+  function check(response,answer){
+    if(response === answer){
+      alert('Correct!');
+      score++;
+    } else {
+      alert(`Wrong! The correct answer was ${answer}`);
+    }
+  }
+
+  function gameOver(){
+    alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
   }
 }
 
-alert(`Game Over, you scored ${score} point${score > 1 ? "s" : ""}`);
+start(quiz);
